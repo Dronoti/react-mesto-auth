0598f1f53d5939818a8eefdce4ebd8d848018a50
Andrey Lazarev <dronoti@yandex.ru>
2022-07-13 08:25:23 +0300

    feat: add mobile version

--- src/components/ScreenWithForm.js
+++ src/components/ScreenWithForm.js
@@ -2,7 +2,7 @@ export default function ScreenWithForm(props) {
     return (
         <div className="form">
             <h2 className="popup__title form__title">{props.title}</h2>
-            <form className="popup__form" name={props.name} onSubmit={props.onSubmit} noValidate>
+            <form className="popup__form form__container" name={props.name} onSubmit={props.onSubmit} noValidate>
                     {props.children}
                 <button className={`popup__save form__button ${!props.isValidForm && 'form__button_inactive'}`}
                         type="submit" disabled={!props.isValidForm} name="buttonSubmit">
